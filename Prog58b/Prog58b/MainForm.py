593fd0112b53e60cf32050180bf62bf2e6ea7098
owenreal <162478543+owenreal@users.noreply.github.com>
2024-03-11 10:12:13 -0500

    3/11

--- Prog58b/Prog58b/MainForm.py
+++ Prog58b/Prog58b/MainForm.py
@@ -10,9 +10,23 @@ class MainForm(Form):
 		self.InitializeComponent()
 	
 	def InitializeComponent(self):
+		self._button1 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# button1
+		# 
+		self._button1.Location = System.Drawing.Point(108, 161)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(75, 23)
+		self._button1.TabIndex = 0
+		self._button1.Text = "button1"
+		self._button1.UseVisualStyleBackColor = True
 		# 
 		# MainForm
 		# 
+		self.ClientSize = System.Drawing.Size(390, 307)
+		self.Controls.Add(self._button1)
 		self.Name = "MainForm"
 		self.Text = "Prog58b"
+		self.ResumeLayout(False)
 
