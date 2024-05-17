40b61c6394dc761f6d87d2da0fb00dea9230972f
owenreal <162478543+owenreal@users.noreply.github.com>
2024-05-17 10:19:05 -0500

    project made

---
+++ Prog 1 final/Prog 1 final/MainForm.py
@@ -0,0 +1,20 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self.SuspendLayout()
+		# 
+		# MainForm
+		# 
+		self.ClientSize = System.Drawing.Size(484, 461)
+		self.Name = "MainForm"
+		self.Text = "Prog 1 final"
+		self.ResumeLayout(False)
+
